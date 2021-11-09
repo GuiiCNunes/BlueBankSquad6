e85ceb6bfffb753910f59c2302a149ce032991be
Guilherme Nunes <guiicnunes@gmail.com>
2021-11-09 20:50:35 -0300

    Inicio

---
+++ src/test/java/br/com/blueBank6/BlueBank6ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.blueBank6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueBank6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
