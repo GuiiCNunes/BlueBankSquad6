246f6a74484d2879d7e68cc36d8da4f849a0a7ff
viniciusmenolli <viniciusmenolli@gmail.com>
2021-11-17 21:23:20 -0300

    Classe conta atualizada.

--- src/main/java/br/com/blueBank6/models/Conta.java
+++ src/main/java/br/com/blueBank6/models/Conta.java
@@ -1,78 +1,38 @@
 package br.com.blueBank6.models;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
 
 @Entity
-@Table(name="conta")
+@Table(name="contas")
 public class Conta {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name= "conta")
+    @Column(name= "conta_id")
     private Long conta;
-    
-    @Column(name= "numero", length = 45)
+
+    @NotNull
+    @Column(name= "numero", length = 15, nullable = false)
     private String numero;
 
-    @Column(name ="agencia", length = 45)
+    @NotNull
+    @Column(name ="agencia", length = 5, nullable = false)
     private String agencia;
+
 
     @Column(name= "status")
     private Boolean status;
 
-    @Column(name= "saldo")
-    private float saldo;
+    @NotNull
+    @Column(name= "saldo", nullable = false)
+    private BigDecimal saldo;
 
-    @Column(length = 45)
-    private String senha;
+    @NotNull
+    @Column(length = 6, nullable = false)
+    private int senha;
 
-    // Conta deve ter referencia à cliente? (cliente_id)
 
-    public Long getConta() {
-        return conta;
-    }
 
-    public void setConta(Long conta) {
-        this.conta = conta;
-    }
-
-    public String getNumero() {
-        return numero;
-    }
-
-    public void setNumero(String numero) {
-        this.numero = numero;
-    }
-
-    public String getAgencia() {
-        return agencia;
-    }
-
-    public void setAgencia(String agencia) {
-        this.agencia = agencia;
-    }
-
-    public Boolean getStatus() {
-        return status;
-    }
-
-    public void setStatus(Boolean status) {
-        this.status = status;
-    }
-
-    public float getSaldo() {
-        return saldo;
-    }
-
-    public void setSaldo(float saldo) {
-        this.saldo = saldo;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
 }
