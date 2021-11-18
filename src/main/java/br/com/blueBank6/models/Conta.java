75d4556fd4d7c7d48125620f92d95cde8226d2a3
viniciusmenolli <viniciusmenolli@gmail.com>
2021-11-17 21:29:01 -0300

    Classe conta atualizada com Number Format.

--- src/main/java/br/com/blueBank6/models/Conta.java
+++ src/main/java/br/com/blueBank6/models/Conta.java
@@ -1,5 +1,7 @@
 package br.com.blueBank6.models;
 
+import org.springframework.format.annotation.NumberFormat;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
@@ -26,13 +28,59 @@ public class Conta {
     private Boolean status;
 
     @NotNull
-    @Column(name= "saldo", nullable = false)
+    @NumberFormat(style = NumberFormat.Style.CURRENCY, pattern = "#.###00,00")
+    @Column(name= "saldo", nullable = false, columnDefinition = "DECIMAL(12,2) DEFAULT 0.00")
     private BigDecimal saldo;
 
     @NotNull
     @Column(length = 6, nullable = false)
     private int senha;
 
+    public Long getConta() {
+        return conta;
+    }
 
+    public void setConta(Long conta) {
+        this.conta = conta;
+    }
 
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    public BigDecimal getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(BigDecimal saldo) {
+        this.saldo = saldo;
+    }
+
+    public int getSenha() {
+        return senha;
+    }
+
+    public void setSenha(int senha) {
+        this.senha = senha;
+    }
 }
