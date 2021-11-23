3681be3ce59405cdf0fe135e6648b17aeccbf309
Guilherme Nunes <guiicnunes@gmail.com>
2021-11-23 20:03:42 -0300

    DEV: Implementando o saque e deposito da transacao, responsabilidade transferida para o ContaService

--- src/main/java/br/com/blueBank6/service/TransacaoService.java
+++ src/main/java/br/com/blueBank6/service/TransacaoService.java
@@ -1,13 +1,10 @@
 package br.com.blueBank6.service;
 
-import br.com.blueBank6.models.Conta;
 import br.com.blueBank6.models.Transacao;
-import br.com.blueBank6.repository.ContaRepository;
 import br.com.blueBank6.repository.TransacaoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 @Service
@@ -17,15 +14,16 @@ public class TransacaoService {
     private TransacaoRepository repository;
 
     @Autowired
-    private ContaRepository contaRepository;
+    private ContaService contaService;
 
     public void save(Transacao transacao) {
+        contaService.setSaldo(transacao.getTipo(), transacao.getValor(), transacao.getConta().getId());
         repository.save(transacao);
-        Conta conta = contaRepository.getById(transacao.getConta().getId());
-        System.out.println(conta.getSaldo());
-        BigDecimal novoSaldo = conta.getSaldo().add(transacao.getValor());
-        conta.setSaldo(novoSaldo);
-        contaRepository.save(conta);
+//        Conta conta = contaRepository.getById(transacao.getConta().getId());
+//        System.out.println(conta.getSaldo());
+//        BigDecimal novoSaldo = conta.getSaldo().add(transacao.getValor());
+//        conta.setSaldo(novoSaldo);
+//        contaRepository.save(conta);
 
     }
 
