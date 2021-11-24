40fb45b9ae26e7ae99bb51cf32acb2b006e3db53
marianafonsek <mfsouzafonseca@gmail.com>
2021-11-24 15:27:50 -0300

    Swagger atualizado

--- src/main/java/br/com/blueBank6/controller/TransacaoController.java
+++ src/main/java/br/com/blueBank6/controller/TransacaoController.java
@@ -1,12 +1,12 @@
 package br.com.blueBank6.controller;
 
-
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.blueBank6.models.Transacao;
@@ -16,23 +16,18 @@ import br.com.blueBank6.service.TransacaoService;
 @RequestMapping(path = "/transacao")
 public class TransacaoController {
 
-    @Autowired
-    private TransacaoService service;
+	@Autowired
+	private TransacaoService transacaoservice;
 
-    @RequestMapping(method = RequestMethod.POST, value = "/salvar")
-    public String salvar(@RequestBody Transacao transacao) {
-        try {
-            service.save(transacao);
-            return "Transacão concluída.";
-        } catch (Exception e) {
-            String msg = e.getMessage();
-            return msg;
-        }
-    }
+	@PostMapping("/salvar")
+	public ResponseEntity<Object> SalvarTransacao(@RequestBody Transacao transacao) {
+		TransacaoService.save(transacao);
+		return new ResponseEntity<>("Transação efetuada com sucesso", HttpStatus.CREATED);
+	}
 
-    @RequestMapping(method = RequestMethod.GET, value = "/lista")
-    public List<Transacao>listar(Transacao transacao) {
-       return  service.findAll();
-       
-    }
+	@GetMapping("/listar")
+	public ResponseEntity<Object> ListarTransacao() {
+		return new ResponseEntity<>(transacaoservice.getTransacao(), HttpStatus.OK);
+	}
+
 }
