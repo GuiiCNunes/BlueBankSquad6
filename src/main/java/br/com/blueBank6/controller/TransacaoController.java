55fd12651cbd440c257b7da4c65074fb1241cd44
Jessi Kopps <jessicakopps@gmail.com>
2021-11-30 19:46:43 -0300

    SNS AWS

--- src/main/java/br/com/blueBank6/controller/TransacaoController.java
+++ src/main/java/br/com/blueBank6/controller/TransacaoController.java
@@ -1,51 +1,56 @@
 package br.com.blueBank6.controller;
 
+import br.com.blueBank6.models.Cliente;
+import br.com.blueBank6.service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 
-import br.com.blueBank6.models.Transacao;
-import br.com.blueBank6.service.TransacaoService;
-
-@RestController
-@RequestMapping(path = "/transacao")
+@RestController(value = "/")
 public class TransacaoController {
 
 	@Autowired
-	private TransacaoService transacaoservice;
+	private ClienteService service;
 
-	@PostMapping("/salvar")
-	public ResponseEntity<Object> SalvarTransacao(@RequestBody Transacao transacao) {
-		TransacaoService.save(transacao);
-		return new ResponseEntity<>("Transação efetuada com sucesso", HttpStatus.CREATED);
+	@RequestMapping(method = RequestMethod.POST, value = "/salvar")
+	public String salvar(@RequestBody Cliente cliente) {
+		try {
+			service.save(cliente);
+			return "Cliente cadastrado com sucesso";
+
+		} catch (Exception e) {
+			String msg = e.getMessage();
+			return msg;
+		}
+
 	}
 
+	@RequestMapping(method = RequestMethod.PUT, value = "/atualizar/{id}")
+	public String atualizar(@PathVariable long id, @RequestBody Cliente cliente) {
+		try {
+			cliente.setId(id);
 
-  @RequestMapping(method = RequestMethod.POST, value = "/salvar")
-  public String salvar(@RequestBody Transacao transacao) {
-      try {
-          transacao.setData(LocalDateTime.now());
-          service.save(transacao);
-          return "Transacão concluída.";
-      } catch (Exception e) {
-          String msg = e.getMessage();
-          return msg;
-      }
-  }
-  
-	@GetMapping("/listar")
-	public ResponseEntity<Object> ListarTransacao() {
-		return new ResponseEntity<>(transacaoservice.getTransacao(), HttpStatus.OK);
+			service.save(cliente);
+			return "Atualizado com sucesso";
+
+		} catch (Exception e) {
+			String msg = e.getMessage();
+			return msg;
+		}
+
 	}
 
+	@RequestMapping(method = RequestMethod.GET, value = "/listar")
+	public ResponseEntity<List<Cliente>> listar() {
+		return ResponseEntity.ok(service.findAll());
+
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/delete/{id}")
+	public String deletar(@PathVariable long id) {
+		service.delete(id);
+		return "Cliente deletado com sucesso";
+	}
 }
