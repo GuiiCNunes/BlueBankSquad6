df7193dfd3f8c34c5809e12218f92143e6298da1
Jessi Kopps <jessicakopps@gmail.com>
2021-11-30 20:28:22 -0300

    SNS AWS

---
+++ src/main/java/br/com/blueBank6/config/SnsAwsConfig.java
@@ -0,0 +1,36 @@
+
+
+package br.com.blueBank6.config;
+
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.sns.AmazonSNSClient;
+import com.amazonaws.services.sns.AmazonSNSClientBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@EnableWebMvc
+@Configuration
+public class SnsAwsConfig {
+
+	public static final String SECRET_KEY = "";
+	public static final String ACCESS_KEY = "";
+
+
+	@Bean
+	@Primary
+	public AmazonSNSClient getSnsClient() {
+		return (AmazonSNSClient) AmazonSNSClientBuilder.standard().withRegion(Regions.US_EAST_1)
+				.withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(ACCESS_KEY, SECRET_KEY))).build();
+	}
+}
